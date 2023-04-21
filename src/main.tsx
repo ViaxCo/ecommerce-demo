6ad76682114393f0b0a6132da085d9c65622d757
Victor Ajibade <ajibadevic@gmail.com>
2023-04-21 14:10:58 +0100

    Add theming and style files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import theme from "./theme";
+// Remove blue outline from buttons and links
+import "focus-visible/dist/focus-visible";
+// Lazy load images
+import "lazysizes";
+// import a plugin
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
---
+++ src/theme/components/tabs.ts
@@ -0,0 +1,12 @@
+const tabs = {
+  sizes: {
+    sm: {
+      tab: {
+        py: "0.375rem",
+        px: "1rem",
+        fontSize: "0.85rem",
+      },
+    },
+  },
+};
+export default tabs;
