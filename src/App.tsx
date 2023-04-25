7933417d33b72a2580af811d3ee2e7e06488f8ae
Victor Ajibade <ajibadevic@gmail.com>
2023-04-25 03:40:24 +0100

    Add Saved Products page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Container from "./components/Container";
 import ProgressLine from "./components/Loading/ProgressLine";
 import { Provider } from "./context/GlobalState";
+import Saved from "./pages/Saved";
 
 const Home = loadable(() => import("./pages/Home"), {
   fallback: <ProgressLine />,
@@ -19,6 +20,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products/:id" element={<Product />} />
+            <Route path="/saved" element={<Saved />} />
           </Routes>
         </Container>
       </Router>
---
+++ src/pages/Saved.tsx
@@ -0,0 +1,33 @@
+import { Text } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+import { useContext } from "react";
+import Main from "../components/Main";
+import ProductCard from "../components/ProductCard";
+import ProductsGrid from "../components/ProductsGrid";
+import { GlobalContext } from "../context/GlobalState";
+
+const Saved = () => {
+  const { products } = useContext(GlobalContext);
+  const savedProducts = products?.filter(product => product.isSaved === true);
+  return (
+    <Main>
+      <ProductsGrid>
+        <AnimatePresence>
+          {savedProducts?.length === 0 ? (
+            <Text>No saved items</Text>
+          ) : (
+            savedProducts!.map(product => (
+              <ProductCard
+                key={product.id}
+                product={product}
+                className="loading-product"
+              />
+            ))
+          )}
+        </AnimatePresence>
+      </ProductsGrid>
+    </Main>
+  );
+};
+
+export default Saved;
